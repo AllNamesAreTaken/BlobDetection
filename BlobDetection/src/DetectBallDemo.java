b42725faf0010877563c9273bbe95d36bf5a121f
holderwd93 <trialwdh@gmail.com>
2014-04-06 17:07:45 -0500

    Dilate was not being used, now it is working

--- BlobDetection/src/DetectBallDemo.java
+++ BlobDetection/src/DetectBallDemo.java
@@ -23,23 +23,23 @@ public class DetectBallDemo {
 		capture.set(3, sizex - 100);
 		capture.set(4, sizey - 100);
 	    
-		Mat image = Highgui.imread("res/TestPic.png");
-		Mat image2 = new Mat();
-		Mat image3 = new Mat();
-		Mat image4 = new Mat();
+		Mat oriImg = Highgui.imread("res/TestPic.png");
+		Mat hsvImg = new Mat();
+		Mat redGsImg = new Mat();
+		Mat redCropImg = new Mat();
 
-		Panel panel1 = new Panel();
-		Frame frame1 = new Frame(panel1, "Image", image.width(), image.height());
+		Panel oriPanel = new Panel();
+		Frame oriFrame = new Frame(oriPanel, "Image", oriImg.width(), oriImg.height());
 	    
 
-		Panel panel2 = new Panel();
-		Frame frame2 = new Frame(panel2, "HSV", image.width(), image.height());
+		Panel hsvPanel = new Panel();
+		Frame hsvFrame = new Frame(hsvPanel, "HSV", oriImg.width(), oriImg.height());
 	    
-		Panel panel3 = new Panel();
-		Frame frame3 = new Frame(panel3, "Grayscale", image.width(), image.height());
+		Panel redGsPanel = new Panel();
+		Frame redGsFrame = new Frame(redGsPanel, "Grayscale", oriImg.width(), oriImg.height());
 		
-		Panel panel4 = new Panel();
-		Frame frame4 = new Frame(panel4, "CroppedBall", image.width(), image.height());
+		Panel redCropPanel = new Panel();
+		Frame redCropFrame = new Frame(redCropPanel, "CroppedBall", oriImg.width(), oriImg.height());
 
 		/*
 		H: 0 - 180
@@ -52,23 +52,23 @@ public class DetectBallDemo {
         Mat erode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
         Mat dilate = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5,5));
         
-		capture.read(image);
-		int cameraWidth = image.width();
-		int cameraHeight = image.height();
+		capture.read(oriImg);
+		int cameraWidth = oriImg.width();
+		int cameraHeight = oriImg.height();
         
 		while(Frame.isOpen) {	
-			Scalar hsv_min = new Scalar(Frame.aColor, 100, 50, 0);
+			Scalar hsv_min = new Scalar(Frame.aColor, 70, 50, 0);
 			Scalar hsv_max = new Scalar(Frame.aColorW, 255, 255, 0);
-			capture.read(image);
-			Imgproc.GaussianBlur(image, image, new Size(19,19), 300.0);
-			Imgproc.cvtColor(image, image2, Imgproc.COLOR_BGR2HSV);
+			capture.read(oriImg);
+			Imgproc.GaussianBlur(oriImg, oriImg, new Size(11,11), 30.0);
+			Imgproc.cvtColor(oriImg, hsvImg, Imgproc.COLOR_BGR2HSV);
 
-			Core.inRange(image2, hsv_min, hsv_max, image3);
-			Imgproc.erode(image3, image3, erode);
-			Imgproc.erode(image3, image3, erode);
+			Core.inRange(hsvImg, hsv_min, hsv_max, redGsImg);
+			Imgproc.erode(redGsImg, redGsImg, erode);
+			Imgproc.erode(redGsImg, redGsImg, erode);
 			
-			Imgproc.dilate(erode, erode, dilate);
-			Imgproc.dilate(erode, erode, dilate);
+			Imgproc.dilate(redGsImg, redGsImg, dilate);
+			Imgproc.dilate(redGsImg, redGsImg, dilate);
 			
 	        //HoughCircles:
 	        	//Image as Mat
@@ -83,7 +83,7 @@ public class DetectBallDemo {
 	        	//Minimum Radius of circle (20)
 	        	//Maximum Radius of circle (100)
 	        Mat circles = new Mat();
-			Imgproc.HoughCircles(image3, circles, Imgproc.CV_HOUGH_GRADIENT, 2, 500, 40, 40, 20, 100);
+			Imgproc.HoughCircles(redGsImg, circles, Imgproc.CV_HOUGH_GRADIENT, 2, 500, 40, 40, 20, 100);
 			if(circles.cols() > 0) 
 			{
 				bi[0] = circles.get(0, 0)[0];
@@ -118,15 +118,15 @@ public class DetectBallDemo {
 				}
 				
 				Rect rec = new Rect(rx, ry, rw, rh);
-				image4 = new Mat(image, rec);
-			    panel4.setimagewithMat(image4);
-			    frame4.repaint();
+				redCropImg = new Mat(oriImg, rec);
+			    redCropPanel.setimagewithMat(redCropImg);
+			    redCropFrame.repaint();
 			}
 			else {
 				bi[0] = -1.0;
 				bi[1] = -1.0;
 				bi[2] = -1.0;
-				image4 = new Mat();
+				redCropImg = new Mat();
 			}
 			Frame.ballPos.setText("x=" + DetectBallDemo.bi[0] + " y=" + DetectBallDemo.bi[1]);
 			for(int i = 0; i < circles.cols(); i++) {
@@ -137,16 +137,16 @@ public class DetectBallDemo {
 				pt.y = Math.round(vCircle[1]);
 				
 				
-				Core.circle(image, pt, rCircle, new Scalar(0,255,0), 5);
+				Core.circle(oriImg, pt, rCircle, new Scalar(0,255,0), 2);
 			}
 			
 			System.out.println(circles.dump());
-		    panel1.setimagewithMat(image);
-		    frame1.repaint();
-		    panel2.setimagewithMat(image2);
-		    frame2.repaint();
-		    panel3.setimagewithMat(image3);
-		    frame3.repaint();
+		    oriPanel.setimagewithMat(oriImg);
+		    oriFrame.repaint();
+		    hsvPanel.setimagewithMat(hsvImg);
+		    hsvFrame.repaint();
+		    redGsPanel.setimagewithMat(redGsImg);
+		    redGsFrame.repaint();
 		}
         
 	    //Successful update from desktop!
